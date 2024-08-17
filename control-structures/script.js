4102f13677582ca106607a0c33c6d8c176122ce2
Spyke Lionel <social.lionel@gmail.com>
2024-08-17 10:44:40 +0100

    do while

--- control-structures/script.js
+++ control-structures/script.js
@@ -6,14 +6,45 @@ document.write("Welcome to CS");
 // Write a program that will alert me to get an umbrela if it is going to rain today
 // criteria: temperature in celsius
 
-const ROOM_TEMPERATURE = 25;
-// if temperate is below room temperature.
-// then it will rain
-// else it won't rain
-let temperature = prompt("Enter temperature in celsius");
+// const ROOM_TEMPERATURE = 25;
+// // if temperate is below room temperature.
+// // then it will rain
+// // else it won't rain
+// let temperature = prompt("Enter temperature in celsius");
 
-if (temperature < ROOM_TEMPERATURE) {
-  alert("Get an umbrella today. It's going to rain");
-} else {
-  alert("It's not going to rain today");
+// if (temperature < ROOM_TEMPERATURE) {
+//   alert("Get an umbrella today. It's going to rain");
+// } else {
+//   alert("It's not going to rain today");
+// }
+
+// 3. Loops
+var fruits = ["Banana", "Apple", "Mango", "Cherry", "Orange"];
+fruits = "Banana, Apple, Mango,";
+
+// For loops
+
+document.write("<h4>For Loops</h4>");
+for (i = 0; i < fruits.length; i++) {
+  document.write(fruits[i] + "<br>");
 }
+document.write("<br>");
+// while
+document.write("<h4>While Loops</h4>");
+
+var j = 0;
+while (j < fruits.length) {
+  document.write(fruits[j] + "<br>");
+  j++;
+}
+
+document.write("<br>");
+// while
+document.write("<h4>Do While Loops</h4>");
+// do while loops
+
+var k = 0;
+do {
+  document.write(fruits[k] + "<br>");
+  k++;
+} while (k < fruits.length);
