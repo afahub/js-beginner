36a6895758975919b484a39c4b9255f4f9068b7a
Spyke Lionel <social.lionel@gmail.com>
2024-08-17 10:52:37 +0100

    using functions in JS

---
+++ functions/script.js
@@ -0,0 +1,29 @@
+// Simple functions
+
+// a simple function that prints my name on the screen
+
+// function printName(name) {
+//   document.write("<h3> Your name is: " + name + "</h3> ");
+// }
+
+// const name = prompt("Enter your name:");
+// printName(name);
+
+// Temperature program before
+
+const ROOM_TEMPERATURE = 25;
+function determineIfItShallRainToday(temperature) {
+  // if temperate is below room temperature.
+  // then it will rain
+  // else it won't rain
+
+  if (temperature < ROOM_TEMPERATURE) {
+    alert("Get an umbrella today. It's going to rain");
+  } else {
+    alert("It's not going to rain today");
+  }
+}
+let temperature = prompt("Enter temperature in celsius");
+33333;
+
+determineIfItShallRainToday(temperature);
